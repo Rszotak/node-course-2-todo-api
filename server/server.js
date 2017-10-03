313b3c7f15c309254c5ce487e1c172b0defa0ffe
Ryan Szotak <ryan.szotak@gmail.com>
2017-10-02 20:04:20 -0700

    added query by ID for todos

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var {mongoose} = require("./db/mongoose.js");
+var {ObjectId} = require("mongodb");
 var {Todo} = require("./models/todo.js");
 var {User} = require("./models/user.js");
 
@@ -26,6 +27,23 @@ app.get("/todos", (req, res) => {
         res.send({todos: todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+//GET /todos/<id>
+app.get("/todos/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectId.isValid(id)){
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            res.status(404).send();
+        }
+        res.send({todo: todo});
+    }, (e) => {
+        res.status(400).send();
     });
 });
 
